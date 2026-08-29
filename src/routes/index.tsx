21064dddb3cd9a4ba84b6b673d6261f9ad256789
Ali <ali.hussein.pre@gmail.com>
2026-08-29 12:01:49 +0200

    fix: update social media links in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -36,8 +36,8 @@ const projects = [
 
 const links = [
   { label: "Email", href: "mailto:ali.hussein.pre@gmail.com" },
-  { label: "GitHub", href: "https://github.com/Ali-Hussein-dev" },
-  { label: "X", href: "https://x.com/AliHussein_20" },
+  { label: "GitHub", href: "https://github.com/alibey-10" },
+  { label: "X", href: "https://x.com/Alibey_10" },
   {
     label: "LinkedIn",
     href: "https://www.linkedin.com/in/%F0%9F%92%BB-ali-h-4292a6156/",
